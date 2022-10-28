2c8f659148f97214a93d78073bf4ac27276932d3
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2022-10-28 16:45:59 +0100

    new changes

--- DemoAPI/Controllers/PeopleController.cs
+++ DemoAPI/Controllers/PeopleController.cs
@@ -8,10 +8,14 @@ using System.Web.Http;
 
 namespace DemoAPI.Controllers
 {
+    /// <summary>
+    /// This is where I give all information about my peeps
+    /// </summary>
     public class PeopleController : ApiController
     {
         // GET: api/People
         List<Person> people = new List<Person>();
+       
         public PeopleController()
         {
             people.Add(new Person { Firstname = "Abdul", LastName = "Salam", Id = 0 });
@@ -39,5 +43,42 @@ namespace DemoAPI.Controllers
         public void Delete(int id)
         {
         }
+        /// <summary>
+        /// Returns all the first names without any input parameters
+        /// </summary>
+        /// <returns>The list of all the first names</returns>
+        [Route("api/People/GetFirstName") ]
+        [HttpGet]
+        public List<string> GetFirstNames()
+        {
+            List<string> output = new List<string>();
+
+            foreach(Person p in people)
+            {
+                output.Add(p.Firstname);
+            }
+            return output;
+
+        }
+        /// <summary>
+        /// returns first name based on userId and age of the person
+        /// </summary>
+        /// <param name="userId"></param> a unique identifier for this person
+        /// <param name="age"></param> the age of the person
+        /// <returns>A list of first names</returns>
+        [Route("api/People/GetFirstNameWithPrm/{userId:int}/{age:int}")]
+        [HttpGet]
+        public List<string> GetFirstNamesWithPrm(int userId, int age)
+        {
+            List<string> output = new List<string>();
+
+            foreach (Person p in people)
+            {
+                output.Add(p.Firstname);
+            }
+            return output;
+
+        }
+
     }
 }
--- DemoAPI/Models/Person.cs
+++ DemoAPI/Models/Person.cs
@@ -5,10 +5,23 @@ using System.Web;
 
 namespace DemoAPI.Models
 {
+    
+    /// <summary>
+    /// Represents one specific person
+    /// </summary>
     public class Person
     {
+        /// <summary>
+        /// Id from SQL
+        /// </summary>
         public int Id { get; set; } = 0;
+        /// <summary>
+        /// users first name
+        /// </summary>
         public string Firstname { get; set; } = "";
+        /// <summary>
+        /// users Last Name
+        /// </summary>
         public string LastName { get; set; } = "";
     }
 }
